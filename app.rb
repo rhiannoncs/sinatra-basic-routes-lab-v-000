e4a2d3ed263226e5b03cfc88483dbde319430fc2
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-03-01 15:39:11 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+    "My name is Rhiannon."
+  end
+
+  get '/hometown' do
+    "My hometown is Columbia, Missouri."
+  end
+
+  get '/favorite-song' do
+    "My favorite song is 'This Must Be the Place.'"
+  end
 end
